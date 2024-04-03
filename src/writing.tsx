72dcc8635a2df62cf6cd092068e75518060aa15b
ShawnSomething <107730112+ShawnSomething@users.noreply.github.com>
2024-04-03 20:35:46 +1100

    made a box

--- src/writing.tsx
+++ src/writing.tsx
@@ -1,5 +1,17 @@
 export const Writing = () => {
     return (
+        <>
         <h2>Shawn Wrote Something</h2>
+        <div className="Deck-box">
+            <div className="Box">
+                <div className="Box-face" id="front">Front</div>
+                <div className="Box-face" id="back">Back</div>
+                <div className="Box-face" id="left">Left</div>
+                <div className="Box-face" id="right">Right</div>
+                <div className="Box-face" id="top">Top</div>
+                <div className="Box-face" id="bottom">bottom</div>
+            </div>
+        </div>
+        </>
     )
 }
