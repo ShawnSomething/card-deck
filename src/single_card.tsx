3d4c7f26505f0ba1122c79eea38635cb44850760
ShawnSomething <107730112+ShawnSomething@users.noreply.github.com>
2024-04-14 13:05:31 +1000

    single card styling

--- src/single_card.tsx
+++ src/single_card.tsx
@@ -5,9 +5,9 @@ export const SingleCard = () => {
                 <div className="card-face" id="front">
                     <div className="card-face" id="project-name">hi</div>
                     <div className="card-face" id="project-thumbnail"></div>
-                    <div className="card-face" id="project-link"></div>
-                    <div className="card-face" id="project-desc">hi</div>
-                    <div className="card-face" id="project-date">hi</div>
+                    <div className="card-face" id="project-link">heya</div>
+                    <div className="card-face" id="project-desc">you</div>
+                    <div className="card-face" id="project-date">cunt</div>
                 </div>
             </div>
         </>
