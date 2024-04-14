d6b488ba478aacc7784964c52fe914bfe381c7b9
ShawnSomething <107730112+ShawnSomething@users.noreply.github.com>
2024-04-14 10:09:15 +1000

    initial slice animation for box opening

--- src/writing.tsx
+++ src/writing.tsx
@@ -4,7 +4,7 @@ import React, { useRef, useState } from 'react';
 export const Writing: React.FC = () => {
     const boxRef = useRef<HTMLDivElement>(null);
     const [isHovered, setIsHovered] = useState(false);
-    const [isClicked, setIsClicked] = useState(false);
+    const [isSparkActive, setIsSparkActive] = useState(false);
 
     const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
         const box = boxRef.current;
@@ -29,8 +29,9 @@ export const Writing: React.FC = () => {
         setIsHovered(false);
     };
 
-    const handleOnClick = () => {
-        setIsClicked(prevState => !prevState);
+    const handleBoxClick = () => {
+        setIsSparkActive(true);
+        setTimeout(() => setIsSparkActive(false), 1000);
     };
 
     return (
@@ -39,7 +40,7 @@ export const Writing: React.FC = () => {
                 <div className="Overlay" style={{ visibility: isHovered ? 'visible' : 'hidden' }} />
                 <div className="Glow" style={{ visibility: isHovered ? 'visible' : 'hidden' }} />
                 <div className="Deck-box">
-                    <div className="Box" ref={boxRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} onClick={handleOnClick}>
+                    <div className="Box" ref={boxRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} onClick={handleBoxClick}>
                         <div className="Box-face" id="front" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}>
                             <div className="text-container">Shawn Wrote Something</div>
                         </div>
@@ -47,7 +48,7 @@ export const Writing: React.FC = () => {
                         <div className="Box-face" id="left" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
                         <div className="Box-face" id="right" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
                         <div className="Box-face" id="top" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}>
-                            <div className={`spark ${isClicked ? 'show' : ''}`} />
+                            {isSparkActive && <div className="spark-animation" />}
                             -------------------
                         </div>
                         <div className="Box-face" id="bottom" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
