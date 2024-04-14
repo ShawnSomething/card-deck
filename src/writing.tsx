e78570dff30e8ff06aa3dcafe23e5c2c11df93d2
ShawnSomething <107730112+ShawnSomething@users.noreply.github.com>
2024-04-14 11:09:30 +1000

    box slice animation

--- src/writing.tsx
+++ src/writing.tsx
@@ -1,63 +1,139 @@
-import React, { useRef, useState } from 'react';
-
+import React, { useRef, useState } from "react";
 
 export const Writing: React.FC = () => {
-    const boxRef = useRef<HTMLDivElement>(null);
-    const [isHovered, setIsHovered] = useState(false);
-    const [isSparkActive, setIsSparkActive] = useState(false);
+  const boxRef = useRef<HTMLDivElement>(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isSparkActive, setIsSparkActive] = useState(false);
+  const [isSliceActive, setIsSliceActive] = useState(false);
+  const [isParticleActive, setIsParticleActive] = useState(false);
 
-    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-        const box = boxRef.current;
-        if (!box) return;
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const box = boxRef.current;
+    if (!box) return;
 
-        const boundingRect = box.getBoundingClientRect();
-        const mouseX = e.clientX - boundingRect.left;
-        const mouseY = e.clientY - boundingRect.top;
+    const boundingRect = box.getBoundingClientRect();
+    const mouseX = e.clientX - boundingRect.left;
+    const mouseY = e.clientY - boundingRect.top;
 
-        const tiltX = (mouseX - boundingRect.width / 2) / boundingRect.width * 45;
-        const tiltY = (mouseY - boundingRect.height / 2) / boundingRect.height * 45;
+    const tiltX = ((mouseX - boundingRect.width / 2) / boundingRect.width) * 45;
+    const tiltY =
+      ((mouseY - boundingRect.height / 2) / boundingRect.height) * 45;
 
-        box.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
-        setIsHovered(true);
-    };
+    box.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
+    setIsHovered(true);
+  };
 
-    const handleMouseLeave = () => {
-        const box = boxRef.current;
-        if (!box) return;
+  const handleMouseLeave = () => {
+    const box = boxRef.current;
+    if (!box) return;
 
-        box.style.transform = 'rotateX(0deg) rotateY(0deg)';
-        setIsHovered(false);
-    };
+    box.style.transform = "rotateX(0deg) rotateY(0deg)";
+    setIsHovered(false);
+  };
 
-    const handleBoxClick = () => {
-        setIsSparkActive(true);
-        setTimeout(() => setIsSparkActive(false), 1000);
-    };
+  const handleBoxClick = () => {
+    setIsSparkActive(true);
+    setTimeout(() => setIsSparkActive(false), 1000);
 
-    return (
-        <>
-            <div className="relative">
-                <div className="Overlay" style={{ visibility: isHovered ? 'visible' : 'hidden' }} />
-                <div className="Glow" style={{ visibility: isHovered ? 'visible' : 'hidden' }} />
-                <div className="Deck-box">
-                    <div className="Box" ref={boxRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} onClick={handleBoxClick}>
-                        <div className="Box-face" id="front" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}>
-                            <div className="text-container">Shawn Wrote Something</div>
-                        </div>
-                        <div className="Box-face" id="back" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}>Back</div>
-                        <div className="Box-face" id="left" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
-                        <div className="Box-face" id="right" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
-                        <div className="Box-face" id="top" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}>
-                            {isSparkActive && <div className="spark-animation" />}
-                            -------------------
-                        </div>
-                        <div className="Box-face" id="bottom" style={{backgroundImage: 'linear-gradient(to bottom right, black, grey)'}}></div>
-                    </div>
-                </div>
+    setIsSliceActive(true);
+    setTimeout(() => setIsSliceActive(false), 1500);
+
+    setIsParticleActive(true);
+    setTimeout(() => setIsParticleActive(false), 1800);
+  };
+
+  return (
+    <>
+      <div className="relative">
+        <div
+          className="Overlay"
+          style={{ visibility: isHovered ? "visible" : "hidden" }}
+        />
+        <div
+          className="Glow"
+          style={{ visibility: isHovered ? "visible" : "hidden" }}
+        />
+        <div className="Deck-box">
+          <div
+            className="Box"
+            ref={boxRef}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleBoxClick}
+          >
+            <div
+              className="Box-face"
+              id="front"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            >
+              <div className="text-container">Shawn Wrote Something</div>
             </div>
-            <div className={`speech-bubble ${isHovered ? 'show' : ''}`}>
-                <p>Stuff that Shawn Wrote. From learnings to musings, a lot goes through his mind at any given time. Some make it into words, few make it online, many lost. Hope you enjoy these articles and keen to have a discussion on them.</p>
+            <div
+              className="Box-face"
+              id="back"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            >
+              Back
             </div>
-        </>
-    );
+            <div
+              className="Box-face"
+              id="slice"
+            >
+            {isSparkActive && <div className="spark-animation" />}
+            {isSliceActive && <div className="slice-animation" />}
+            {isParticleActive && <div className="particle-animation" />}
+            </div>
+            <div
+              className="Box-face"
+              id="left"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            ></div>
+            <div
+              className="Box-face"
+              id="right"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            ></div>
+            <div
+              className="Box-face"
+              id="top"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            >
+              -------------------
+            </div>
+            <div
+              className="Box-face"
+              id="bottom"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to bottom right, black, grey)",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className={`speech-bubble ${isHovered ? "show" : ""}`}>
+        <p>
+          Stuff that Shawn Wrote. From learnings to musings, a lot goes through
+          his mind at any given time. Some make it into words, few make it
+          online, many lost. Hope you enjoy these articles and keen to have a
+          discussion on them.
+        </p>
+      </div>
+    </>
+  );
 };
