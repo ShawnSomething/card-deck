4f5e544d160e7a749497928987f2731cfe004e6a
ShawnSomething <107730112+ShawnSomething@users.noreply.github.com>
2024-04-16 08:53:34 +1000

    final nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Hero } from './hero';
 import { Writing } from './writing';
 import { Coding } from './coding';
 import { Animating } from './animating';
+import { WritingCards } from './writing_cards';
+import { AnimatingCards } from './animating_cards';
+import { CodingCards } from './coding_cards';
 import { About } from './about';
 import { NavBar } from './nav';
 
--- src/nav.tsx
+++ src/nav.tsx
@@ -1,13 +1,7 @@
 export const NavBar = () => {
   return (
       <header className="Nav-bar">
-          <p className="home">Shawn Something</p>
-          <ul>
-              <li><a href="./src/writing"></a>Writing</li>
-              <li><a href="./src/coding"></a>Coding</li>
-              <li><a href="./src/animating"></a>Animating</li>
-              <li><a href="./src/about"></a>About</li>
-          </ul>
+          <p className="home"><a href="/" style={{color: "white", textDecoration: "none"}}>Shawn Something</a></p>
       </header>
   )
 }
